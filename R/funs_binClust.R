efe74c3f97c8c1e9caefe8b30f3a0eb4768e0842
ShaojunLab <77885127+ShaojunLab@users.noreply.github.com>
2025-06-02 13:06:57 +0800

    Update funs_binClust.R

--- R/funs_binClust.R
+++ R/funs_binClust.R
@@ -50,7 +50,7 @@ binClust <- function(inputMat,cellMeta,ref_group_names=NULL,
   appender_func <- flog.logger()$appender
   log_path <- environment(appender_func)$file
   if(!is.null(log_path)){
-    if(file.exists(log_path)){flog.appender(appender.file(log_path))}
+    if(file.exists(log_path)){flog.appender(appender.file(log_path))}else{flog.appender(appender.console())}
   }else{
     flog.appender(appender.console())
   }
